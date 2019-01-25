5397320ef166994752eaa85c95aebc80937d2b87
dantmnf <dantmnf@users.noreply.github.com>
2019-01-25 22:55:15 +0800

    fix broken RNG

--- CPP/7zip/Crypto/RandGen.h
+++ CPP/7zip/Crypto/RandGen.h
@@ -3,17 +3,28 @@
 #ifndef __CRYPTO_RAND_GEN_H
 #define __CRYPTO_RAND_GEN_H
 
-#include "../../../C/Sha256.h"
+#include "../../../C/7zTypes.h"
+
+#ifdef _WIN32
+typedef BOOL (WINAPI *FuncRtlGenRandom)(void *dest, unsigned long size);
+#endif
 
 class CRandomGenerator
 {
-  Byte _buff[SHA256_DIGEST_SIZE];
+#ifdef _WIN32
+  FuncRtlGenRandom pRtlGenRandom;
+#else
+  int rng_fd;
+#endif
   bool _needInit;
 
   void Init();
 public:
   CRandomGenerator(): _needInit(true) {};
   void Generate(Byte *data, unsigned size);
+#ifndef _WIN32
+  ~CRandomGenerator();
+#endif
 };
 
 extern CRandomGenerator g_RandomGenerator;
